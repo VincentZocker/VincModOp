d7756a6b42f85c6401821319a2dc6ee7a8bff8fa
vincent <vincentzocker@gmail.com>
2020-11-02 12:17:39 +0100

    dein test

--- src/main/java/info/vincent/vincmodop/VincModOp.java
+++ src/main/java/info/vincent/vincmodop/VincModOp.java
@@ -23,7 +23,7 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
+@Mod("vincmod")
 public class VincModOp {
     // Directly reference a log4j logger.
     public static final Logger LOGGER = LogManager.getLogger();
@@ -55,7 +55,7 @@ public class VincModOp {
 
     private void enqueueIMC(final InterModEnqueueEvent event) {
         // some example code to dispatch IMC to another mod
-        InterModComms.sendTo("examplemod", "helloworld", () -> {
+        InterModComms.sendTo("vincmod", "helloworld", () -> {
             LOGGER.info("Hello world from the MDK");
             return "Hello world";
         });
